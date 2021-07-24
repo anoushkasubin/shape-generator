d3891c3363f90c686d081d888451ec7de6e2bccf
anoushkasubin <84610945+anoushkasubin@users.noreply.github.com>
2021-07-24 15:35:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+let button = document.getElementById("button");
+var numberInShape = 1;
+var oldValue = 0;
+
+button.onclick = () => {
+let n = document.getElementById("number").value;
+const circle = document.getElementById("circle");
+const square = document.getElementById("square");
+const rect = document.getElementById("rect");
+const $box = document.getElementById("box");
+
+let j;
+n =  Number(oldValue) + Number(n);
+
+for(let j = numberInShape; j <= n; j++){
+var shape = document.createElement("div");
+shape.innerHTML += numberInShape;
+
+
+if(square.checked){
+    shape.classList.add("square");
+}
+else if(circle.checked){
+    shape.classList.add("circle")
+}
+else if(rect.checked){
+    shape.classList.add("rectangle")
+}
+else{
+    document.write("invalid input");
+}
+
+$box.appendChild(shape);
+numberInShape++;
+oldValue = document.getElementById("box").lastElementChild.innerHTML
+}
+}
